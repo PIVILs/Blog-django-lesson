94a4629ce80e63ba5ef3b0ee8391eca2406d84e7
PIVILs <PIVILs@mail.ru>
2022-06-08 14:35:10 +0400

    add a edit entry

--- blogs/urls.py
+++ blogs/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     # Страница для добавления нового блога
     path('new_blog/', views.new_blog, name='new_blog'),
     # Страница для добавления новой записи
-    path('new_entry/<int:blog_id>/', views.new_entry, name='new_entry')
+    path('new_entry/<int:blog_id>/', views.new_entry, name='new_entry'),
+    # Страница для редактирования записи
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import BlogPost
+from .models import BlogPost, Entry
 from .forms import BlogForm, EntryForm
 
 def index(request):
@@ -51,3 +51,19 @@ def new_entry(request, blog_id):
 
     context = {'blog':blog, 'form': form}
     return render(request, 'blogs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """ Редактирует существующую запись."""
+    entry = Entry.objects.get(id=entry_id)
+    blog = entry.blog
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:blog', blog_id=blog.id)
+
+    context = {'entry': entry, 'blog': blog, 'form': form}
+    return render(request, 'blogs/edit_entry.html', context)
