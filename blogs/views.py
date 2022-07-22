052e6886cda89fc707a0b169ecbb91543a3839bf
PIVILs <PIVILs@mail.ru>
2022-07-22 18:14:28 +0400

    add @login_required and check_blog_user()

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+
 from .models import BlogPost, Entry
 from .forms import BlogForm, EntryForm
 
@@ -6,19 +9,23 @@ def index(request):
     """Домашняя страница приложения Blog"""
     return render(request, 'blogs/index.html')
 
+@login_required
 def all_blogs(request):
     """Выводит срисок всех блогов."""
-    blogs = BlogPost.objects.order_by('date_added')
+    blogs = BlogPost.objects.filter(owner=request.user).order_by('date_added')
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.html', context)
 
+@login_required
 def the_blog(request, blog_id):
     """Выводит один блог и все его записи."""
     blog = BlogPost.objects.get(id=blog_id)
+    check_blog_user(blog, request)
     entries = blog.entry_set.order_by('date_added')
     context = {'blog': blog, 'entries': entries}
     return render(request, 'blogs/blog.html', context)
 
+@login_required
 def new_blog(request):
     """Определяет новый блог."""
     if request.method != 'POST':
@@ -28,6 +35,9 @@ def new_blog(request):
         # Отправлены данные POST, обработать данные.
         form = BlogForm(data=request.POST)
         if form.is_valid():
+            new_blog = form.save(commit=False)
+            new_blog.owner =request.user
+            new_blog.save()
             form.save()
             return redirect('blogs:blogs')
 
@@ -35,9 +45,11 @@ def new_blog(request):
     context = {'form': form}
     return render(request, 'blogs/new_blog.html', context)
 
+@login_required
 def new_entry(request, blog_id):
     """ Добавляет новую запись в конкретном блоге."""
     blog = BlogPost.objects.get(id=blog_id)
+    check_blog_user(blog, request)
 
     if request.method != 'POST':
         form = EntryForm()
@@ -52,10 +64,12 @@ def new_entry(request, blog_id):
     context = {'blog':blog, 'form': form}
     return render(request, 'blogs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """ Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     blog = entry.blog
+    check_blog_user(blog, request)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -67,3 +81,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'blog': blog, 'form': form}
     return render(request, 'blogs/edit_entry.html', context)
+
+def check_blog_user(blog, request):
+    """ Проверка принадлежности блок пользователю."""
+    if blog.owner != request.user:
+        raise Http404
